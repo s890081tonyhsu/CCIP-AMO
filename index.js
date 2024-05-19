7c4d1f1ee6f9859a5c3e78a32356dbaad0008ba3
rr37 <ay870527@gmail.com>
2024-05-19 19:56:05 +0800

    feat: export response to schedule.json

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const generateSchedule = require('./generateSchedule')
-const config = require("./config");
-(async () => {
+const fs = require('fs')
+const config = require('./config')
+;(async () => {
   let res = await generateSchedule(config)
+
+  // 檔案名稱和路徑
+  const fileName = 'schedule.json'
+
+  // 將資料寫入檔案
+  fs.writeFile(fileName, res, (err) => {
+    if (err) {
+      console.error('無法寫入檔案：', err)
+      return
+    }
+    console.log('資料已成功寫入到檔案中。')
+  })
+
   console.log(res)
 })()
