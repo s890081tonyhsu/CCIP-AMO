a1703aff1637074a63d06c0da0fe91b4d152169c
rr37 <ay870527@gmail.com>
2024-05-21 14:44:54 +0800

    feat: render agenda with selected date

--- amo-app/src/scheduleSVG.js
+++ amo-app/src/scheduleSVG.js
@@ -1,13 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOMServer from 'react-dom/server'
 import './ScheduleSVG.css'
 import scheduledata from './schedule.json'
 
 const ScheduleSVG = () => {
+  const [selectedDate, setSelectedDate] = useState(null)
+
   useEffect(() => {
     // 在組件加載完成後輸出資訊
     console.log(scheduledata.speakers)
   }, [])
+
+  // 提取所有的唯一日期
+  const getUniqueDates = (sessions) => {
+    const dates = sessions.map((session) => session.start.split('T')[0])
+    return [...new Set(dates)]
+  }
+
+  // 根據選擇的日期過濾議程
+  const filteredSessions = selectedDate
+    ? scheduledata.sessions.filter((session) =>
+        session.start.includes(selectedDate)
+      )
+    : scheduledata.sessions
 
   const extractTime = (dateTimeString) => {
     const date = new Date(dateTimeString)
@@ -65,7 +80,7 @@ const ScheduleSVG = () => {
               </text>
 
               {/* 英文標題 */}
-              <text x="80" y={yPosition+20} className="title">
+              <text x="80" y={yPosition + 20} className="title">
                 {session.en.title}
               </text>
 
@@ -84,7 +99,9 @@ const ScheduleSVG = () => {
 
   const downloadSVG = () => {
     // 把 React 的元素轉換為正常的 html 字串
-    const svgContent = ReactDOMServer.renderToStaticMarkup(generateSVG({ sessions: scheduledata.sessions }))
+    const svgContent = ReactDOMServer.renderToStaticMarkup(
+      generateSVG({ sessions: filteredSessions })
+    )
     const blob = new Blob([svgContent], { type: 'image/svg+xml' })
     const url = URL.createObjectURL(blob)
     const link = document.createElement('a')
@@ -97,8 +114,16 @@ const ScheduleSVG = () => {
 
   return (
     <div>
+      <div>
+        {/* 生成日期選擇按鈕 */}
+        {getUniqueDates(scheduledata.sessions).map((date, index) => (
+          <button key={index} onClick={() => setSelectedDate(date)}>
+            {date}
+          </button>
+        ))}
+      </div>
       <div className="svg-container">
-        {generateSVG({ sessions: scheduledata.sessions })}
+        {generateSVG({ sessions: filteredSessions })}
       </div>
       <button className="download-button" onClick={downloadSVG}>
         Download as SVG
